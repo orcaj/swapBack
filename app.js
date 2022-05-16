55eb94cc587e46939cae2b3a1b9ae72c4542e3aa
nikola <Nikola.Trifunovic@codemonk.ai>
2022-05-16 15:32:11 +0200

    cors

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-    origin: "http://localhost:3000"
+    origin: process.env.FRONT_URL
 };
-app.use(cors());
+app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
