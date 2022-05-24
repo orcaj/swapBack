ae71f76595c6cf566647f901d3e2cb5171b8b247
nikola <Nikola.Trifunovic@codemonk.ai>
2022-05-24 12:07:00 +0200

    allow all

--- app.js
+++ app.js
@@ -13,14 +13,8 @@ const env = process.env.NODE_ENV || 'local';
 dotenv.config({ path: env + '.env' });
 
 var corsOptions = {
-    origin: process.env.FRONT_URL
-};
-console.log('process.env.FRONT_URL', process.env.FRONT_URL)
-
-app.use(cors(corsOptions));
-
-var corsOptions = {
-    origin: process.env.FRONT_URL
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 app.use(cors(corsOptions));
 
